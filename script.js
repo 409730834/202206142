11ab8d75eaa6d1d77aa28ae13844795fc034380a
409730834 <107436190+409730834@users.noreply.github.com>
2022-06-14 04:19:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+scene
+  width:230px
+  height:200px
+  background-image: url(https://i.imgur.com/3dGP7GR.jpg )0 90% no-repeat
+  background-size: cover
+  background-size: 500% 400%
+  border-radius: 20px
+  position: relative 
+  overflow: hidden
+  transition: background-position 15s linear
+  sence:hover
+  background-position: 150% 90%
+  .mario
+    width: 65px
+    height: 100px
